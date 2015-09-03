3e319e48e291f2908816c449a0b236171c382a8d
unknown <sthak8@DELTAHITI31110.sapient.com>
2015-09-03 14:45:00 +0530

    FirstCommit

---
+++ MiniProject/src/com/sapient/client/cui/Item.java
@@ -0,0 +1,38 @@
+package com.sapient.client.cui;
+
+public class Item {
+
+	Object shippingWeight;
+	object description;
+	Object orderDeal;
+	public Object getShippingWeight() {
+		return shippingWeight;
+	}
+	public void setShippingWeight(Object shippingWeight) {
+		this.shippingWeight = shippingWeight;
+	}
+	public object getDescription() {
+		return description;
+	}
+	public void setDescription(object description) {
+		this.description = description;
+	}
+	public Object getOrderDeal() {
+		return orderDeal;
+	}
+	public void setOrderDeal(Object orderDeal) {
+		this.orderDeal = orderDeal;
+	}
+	
+	public void getPriceForQuantity()
+	{
+		
+	}
+	
+	public void getWeight()
+	{
+	}
+	
+	}
+	
+}
---
+++ MiniProject/src/com/sapient/client/cui/OrderDetail.java
@@ -0,0 +1,46 @@
+package com.sapient.client.cui;
+
+public class OrderDetail {
+
+Object Quantity;
+Object Taxstatus;
+Order order
+Item Item;
+public Object getQuantity() {
+	return Quantity;
+}
+public void setQuantity(Object quantity) {
+	this.Quantity = quantity;
+}
+public Object getTaxstatus() {
+	return Taxstatus;
+}
+public void setTaxstatus(Object taxstatus) {
+	this.Taxstatus = taxstatus;
+}
+public Order getOrder() {
+	return order;
+}
+public void setOrder(Order order) {
+	this.order = order;
+}
+public Item getItem() {
+	return Item;
+}
+public void setItem(Item item) {
+	this.Item = item;
+}
+
+public void calcSubTotal()
+{
+}
+public void calcWeight()
+{
+}
+
+}
+
+
+
+
+
